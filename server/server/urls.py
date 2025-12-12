b1ad671d166d8aea8d8d85656c1c97610bb60a2c
nsaaro <moi@nsaaro.com>
2025-12-12 14:04:15 +0300

    updates

--- server/server/urls.py
+++ server/server/urls.py
@@ -13,7 +13,7 @@ router.register(r'sales', views.SaleViewSet) # router.register(r'api', views.Use
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-     path('', include(router.urls)),
+     path('api', include(router.urls)),
 #     path('api/', include(router.urls)),
 #     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 
