ced0e1383474d38a90082a3a93ad1aabf6f683d7
nsaaro <moi@nsaaro.com>
2026-02-11 15:37:33 +0300

    updates

--- server/home/views/apis/old_apis.py
+++ server/home/views/apis/old_apis.py
@@ -121,7 +121,7 @@ class ProductViewSet(viewsets.ModelViewSet):
             created_by=user, 
             deleted=False
         )
-        print(products.values("created_by", "created_by_id"))  # Debug: Print the actual SQL query being executed
+        print(products.count())  # Debug: Print the actual SQL query being executed
         return products
 
     def perform_create(self, serializer):
